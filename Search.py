e4536a9e5e00a9bf6a2d3bc38a28846c560c6452
Daniel Engbert <end1@umbc.edu>
2017-02-08 23:38:00 -0500

    Initial commit. Args are stored in respective variables.

---
+++ Search.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sys
+
+input_file = sys.argv[1];
+start_node = sys.argv[2];
+end_node = sys.argv[3];
+search_type = sys.argv[4];
+
+print("arg list: " + str(sys.argv))
+print("input_file: " + input_file)
+print("start_node: " + start_node)
+print("end_node: " + end_node)
+print("search_type: " + search_type)
