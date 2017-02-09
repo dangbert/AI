29f197106f3763dc537c4cc757dd1009b72da168
Daniel Engbert <end1@umbc.edu>
2017-02-09 17:11:34 -0500

    Fixed syntax error. Created function for storing the neighbors of each node.

--- Search.py
+++ Search.py
@@ -19,7 +19,6 @@ def readFile(fileName):
     for line in f:
         # split each line (by space character) into array of ints
         vals = map(int, line.split())
-        print(vals)
 
         source = vals[0] # the edge's source node
         dest = vals[1]   # the edge's destination node
@@ -27,6 +26,7 @@ def readFile(fileName):
 
         # store the weight of this edge
         weights[edgeName(source, dest)] = weight
+        writeNode(source, dest) # remember that these nodes are connected
     f.close()
 
 
@@ -43,16 +43,25 @@ def edgeName(id1, id2):
         temp = id1
         id1 = id2
         id2 = temp
-    return id1 + "-" + id2
+    return str(id1) + "-" + str(id2)
 
 
-# @param id1, neightbor: int IDs of the node and its neighbor
-def writeNode(id, neighbor):
-    if id not in nodes:
-        nodes[id] = []
-    nodes[id].append(neighbor)
+# @param id1, id2: int IDs of the node and its neighbor
+def writeNode(id1, id2):
+    if id1 not in nodes:
+        nodes[id1] = []
+    nodes[id1].append(id2)
+
+    if id2 not in nodes:
+        nodes[id2] = []
+    nodes[id2].append(id1)
 
 # need list of already explored/expanded nodes (hash table?)
 # need way to retrieve the neighbors of a node when "building" the search tree
 
 readFile(input_file)
+
+print("\nnodes:")
+print(nodes)
+print("\nweights:")
+print(weights)
