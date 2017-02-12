e201af700b2b635b65ea6a7e6cb8b8c01651e6fc
Daniel Engbert <end1@umbc.edu>
2017-02-12 01:34:59 -0500

    Started a BFS function which traverses the nodes using the queue.

--- Search.py
+++ Search.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 import sys
+from collections import deque
 
 print("arg list: " + str(sys.argv))
 
 # global variables
 input_file = sys.argv[1]
-start_node = sys.argv[2]
-end_node = sys.argv[3]
+start_node = int(sys.argv[2])
+end_node = int(sys.argv[3])
 search_type = sys.argv[4] # 'BFS' or 'DFS', or 'UCS'
 
-nodes = {} # dictionary
-weights = {}
+nodes = {}    # dictionary storing the neighbors of each node
+added = {}    # (dictionary) of already explored/expanded nodes
+weights = {}  # dictionary of the weights of the edges between connected nodes
+q = deque([]) # FIFO queue
 
 # read data from input file and store it
 def readFile(fileName):
@@ -35,6 +38,7 @@ def readFile(fileName):
 def getWeight(id1, id2):
     return weights[edgeName(id1, id2)]
 
+
 # returns the name of the edge containing the two nodes
 # @param id1, id2: int IDs of the nodes
 def edgeName(id1, id2):
@@ -56,8 +60,23 @@ def writeNode(id1, id2):
         nodes[id2] = []
     nodes[id2].append(id1)
 
-# need list of already explored/expanded nodes (hash table?)
-# need way to retrieve the neighbors of a node when "building" the search tree
+
+def BFS(id):
+    print("at node " + str(id))
+
+    for n in nodes[int(id)]:
+        # check that the neighbors haven't been already added
+        if n not in added:
+            print(" -adding " + str(n))
+            q.append(int(n)) # add the connected nodes to the queue
+            added[n] = 1 # indicate that this node is already in the queue
+
+    # when q not empty
+    if q:
+        print(added)
+        BFS(int(q.popleft()))
+
+
 
 readFile(input_file)
 
@@ -65,3 +84,9 @@ print("\nnodes:")
 print(nodes)
 print("\nweights:")
 print(weights)
+
+q.append(start_node)
+added[start_node] = 1
+
+BFS(int(q.popleft()))
+print("\n")
