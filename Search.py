615f032272f9eed257550090b3b20b58070a64c0
Daniel Engbert <end1@umbc.edu>
2017-02-12 02:00:31 -0500

    BFS function now stores parent of each discovered node so that the path can be determined later.

--- Search.py
+++ Search.py
@@ -11,7 +11,9 @@ end_node = int(sys.argv[3])
 search_type = sys.argv[4] # 'BFS' or 'DFS', or 'UCS'
 
 nodes = {}    # dictionary storing the neighbors of each node
+# TODO: consider renaming to 'discovered'
 added = {}    # (dictionary) of already explored/expanded nodes
+              # nodes are paired with the id of their parents
 weights = {}  # dictionary of the weights of the edges between connected nodes
 q = deque([]) # FIFO queue
 
@@ -50,6 +52,8 @@ def edgeName(id1, id2):
     return str(id1) + "-" + str(id2)
 
 
+# TODO: nodes should be added to in numerical order to your expansion data structure.
+#       can we assume the data is provided in numerical order?
 # @param id1, id2: int IDs of the node and its neighbor
 def writeNode(id1, id2):
     if id1 not in nodes:
@@ -62,14 +66,27 @@ def writeNode(id1, id2):
 
 
 def BFS(id):
-    print("at node " + str(id))
+    if id == end_node:
+        print "*** at goal node: " + str(id)
+        res = []
+        cur = id
+
+        while cur != None:
+            res = [cur] + res
+            cur = added[cur]
+
+        print("SOLUTION:")
+        print(res)
+
+    else:
+        print("at node " + str(id))
 
     for n in nodes[int(id)]:
         # check that the neighbors haven't been already added
         if n not in added:
             print(" -adding " + str(n))
             q.append(int(n)) # add the connected nodes to the queue
-            added[n] = 1 # indicate that this node is already in the queue
+            added[n] = id # indicate that this node is already in the queue
 
     # when q not empty
     if q:
@@ -86,7 +103,8 @@ print("\nweights:")
 print(weights)
 
 q.append(start_node)
-added[start_node] = 1
+added[start_node] = None
 
 BFS(int(q.popleft()))
+
 print("\n")
