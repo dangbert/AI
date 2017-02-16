c471ba06adcb11042b3ea0412913871f82e4330b
Daniel Engbert <end1@umbc.edu>
2017-02-16 18:42:19 -0500

    Passing all the tests now. Had to add neighbor nodes to the stack in reverse order for DFS.

--- Search.py
+++ Search.py
@@ -13,9 +13,8 @@ start_node = int(sys.argv[2])
 end_node = int(sys.argv[3])
 search_type = sys.argv[4] # 'BFS' or 'DFS', or 'UCS'
 
-nodes = {}    # dictionary storing the neighbors of each node
-# TODO: consider renaming to 'discovered'
-added = {}    # (dictionary) of already explored/expanded nodes
+nodes = {}    # dictionary storing the neighbors each node can access
+added = {}    # (dictionary) of already discovered nodes
               # nodes are paired with the id of their parents
 
 weights = {}  # dictionary of the weights of the edges between connected nodes
@@ -28,7 +27,6 @@ def readFile(fileName):
     f = open(fileName, "r") # open file for reading
 
     for line in f:
-        ##print(line)
         # split each line (by space character) into array of ints
         vals = list(map(int, line.split()))
 
@@ -59,16 +57,28 @@ def edgeName(id1, id2):
     return str(id1) + "-" + str(id2)
 
 
-# @param id1, id2: int IDs of the node and its neighbor
-def writeNode(id1, id2):
-    if id1 not in nodes:
-        nodes[id1] = []
-    nodes[id1].append(id2)
+# @param src, dest: int IDs of the node and its neighbor
+def writeNode(src, dest):
+    if src not in nodes:
+        nodes[src] = []
+    nodes[src].append(dest)
 
     # this is a directional graph, not bidirectional!
-    #if id2 not in nodes:
-    #    nodes[id2] = []
-    #nodes[id2].append(id1)
+    #if dest not in nodes:
+    #    nodes[dest] = []
+    #nodes[dest].append(src)
+
+
+# prints the final results as an array of node IDs
+def printResults(parent):
+    res = []               # array of the order to visit the nodes
+    temp = end_node
+
+    if end_node in parent:
+        while temp != None:
+            res = [temp] + res
+            temp = parent[temp]
+    print(res)
 
 
 def BFS(id):
@@ -79,44 +89,33 @@ def BFS(id):
                 q.append(int(n)) # add the connected nodes to the queue
                 added[n] = id    # indicate that this node is already in the queue
                                  # and store its parent
-
     # when q not empty
     if q:
         BFS(int(q.popleft()))
-
     else:
-        res = []
-        cur = end_node
+        printResults(added)
 
-        while cur != None:
-            res = [cur] + res
-            cur = added[cur]
 
-        print(res)
-
+# TODO: combine these functions (they're the same except q and stack are diff names)
+# maybe make a helper function?
 def DFS(id):
-    #print(" DFS, ID = " + str(id))
+    found = False
     if id in nodes:
-        for n in nodes[int(id)]:
+        for n in reversed(nodes[int(id)]):
             # check that the neighbors haven't been already added
             if n not in added:
-                stack.append(int(n)) # add the connected nodes to the queue
-                added[n] = id    # indicate that this node is already in the queue
-    #else:
-    #    print(" ... not in nodes")
+                # TODO: consider using set instead of dictionary (for added)
+                added[n] = 1        # indicate that this node is already in the queue
+                stack.append(int(n))  # add the connected nodes to the queue
+                parent[n] = id # update parent
+                if n == end_node:
+                    found = True
+                    break
 
-    if stack:
+    if stack and not found:                         # stack not empty
         DFS(int(stack.pop()))
-
     else:
-        res = []
-        cur = end_node
-
-        while cur != None:
-            res = [cur] + res
-            cur = added[cur]
-
-        print(res)
+        printResults(parent)
 
 
 def UCS():
@@ -129,27 +128,18 @@ def UCS():
         try:
             cur_cost = pq[pq.top()]
             cur = pq.pop() # ID of the node that we're currently visiting
-            #print("\npopped " + str(cur) + ", " + str(cur_cost))
 
             for n in nodes[int(cur)]: # neighbors of current node
                 if n not in visited:
-                    #print(" -at neighbor " + str(n))
                     # calculate the distance to n
                     n_cost = cur_cost + getWeight(cur, n)
-                    #print("    cost to " + str(n) + ": " + str(n_cost))
 
                     if n in pq:
-                        #print("    already in pq")
                         if n_cost < pq[n]:
-                            pq[n] = n_cost # update priority
-                            parent[n] = cur # update parent
-                            #print("    *updated priority")
-                        #else:
-
-                            #print("    " + str(n) + " has lower (existing) cost " + str(pq[n]))
+                            pq[n] = n_cost   # update priority
+                            parent[n] = cur  # update parent
                     else:
                         # add to priority queue
-                        #print("    ADDING to queue")
                         pq[n] = n_cost
                         parent[n] = cur
 
@@ -157,20 +147,11 @@ def UCS():
             if cur == end_node:
                 break
 
-
-            #pq['a'] = 99 # updates the priority of 'a'
-
-        except KeyError:
+        except KeyError:   # when the priority queue is empty
             break
 
     # queue is now empty
-    res = []
-    temp = end_node
-
-    while temp != None:
-        res = [temp] + res
-        temp = parent[temp]
-    print(res)
+    printResults(parent)
 
 
 
@@ -186,12 +167,11 @@ if search_type == "BFS":
     BFS(int(q.popleft()))
 
 if search_type == "DFS":
+    parent = {start_node: None}
+
     stack.append(start_node)
-    added[start_node] = None
+    added[start_node] = 1
     DFS(int(stack.pop()))
 
 if search_type == "UCS":
     UCS()
-
-
-##print("\n")
