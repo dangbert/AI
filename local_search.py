3feffb29187d4034e63fd7db675a3647081a0304
Daniel Engbert <end1@umbc.edu>
2017-02-24 13:16:19 -0500

    Replaced local_search.py with the provided version, and added comments to it.

--- local_search.py
+++ local_search.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
-from prettytable import PrettyTable
 from schedule import Schedule
-print("in proj2")
+
+# implement the hill climbing search algorithm for these schedules. Try each
+# different heuristic to see how better schedules are made with better
+# heursitics. A state in this problem is just one instance of a schedule, and
+# you can change the state by changing who works what shift on what day. I
+# recommend changing a state one shift at a time to see the algorithm work
+# well. For hill climbing to work you want to figure out what change to the
+# current schedule results in a better schedule according to the heuristic you
+# are using.
+
+def main():
+    #do the thing
+    pass
+
+if __name__ == "__main__":
+    main()
