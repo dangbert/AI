a0d5f543ec344713fc8f0c313c416d777bda7bea
Daniel Engbert <end1@umbc.edu>
2017-04-24 20:05:03 -0400

    Created _getLabelProportion in DecisionTree.

--- DecisionTree/__init__.py
+++ DecisionTree/__init__.py
@@ -14,28 +14,42 @@ class DecisionTree:
     # len(data) must == len(labels)
     def train(self, data, labels):
         self._numAttributes = len(data[0])
-        numPoints = len(data)                   # number of (training) data points
+        self.numPoints = len(data)              # number of (training) data points
 
         # array of empty dictionaries
-        counts = [{} for i in range(self._numAttributes)]
+        self._counts = [{} for i in range(self._numAttributes)]
+        self._label_counts = {}                       # number of times each label is used
 
-        # populate counts of each value seen
-        # use these to calculate probabilities later
-        for v in data:                          # iterate over each vector
+        for n in range(len(data)):              # iterate over each vector (data point)
+            # count number of occurences of each label value
+            if not labels[n] in self._label_counts:
+                self._label_counts[labels[n]] = 0
+            self._label_counts[labels[n]] += 1
+
+            v = data[n]
+            # for each attribute, count the number of times each value is seen
             for i in range (self._numAttributes):
                 val = v[i]
-                if not val in counts[i]:
-                    counts[i][val] = 0
-                counts[i][val] += 1
+                if not val in self._counts[i]:
+                    self._counts[i][val] = 0
+                self._counts[i][val] += 1
 
-        print(counts)
+        print(self._counts)
+        print(self._label_counts)
 
 
     # get probability of a specified attrbute having a given value
     def _getProportion(self, attr, val):
-        if not val in counts[attr]:
+        if not val in self._counts[attr]:
             return 0.0
-        return counts[attr][val] / self._numAttributes;
+        return self._counts[attr][val] / self._numAttributes;
+
+
+    # get proportion of a specific label value amongst all classifications
+    def _getLabelProportion(self, label):
+        if not label in self._label_counts:
+            return 0.0
+        return self._label_counts[label] / self._numPoints;
 
 
     # classify a vector (after training has been completed)
