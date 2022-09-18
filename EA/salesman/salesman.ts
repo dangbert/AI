afc7090769daa4378c2ab1886d368a1aa692fd36
Daniel Engbert <end1@umbc.edu>
2022-09-18 13:07:56 +0200

    Init salesman.ts (EA) project.

---
+++ EA/salesman/salesman.ts
@@ -0,0 +1,55 @@
+console.log('hello world');
+
+const points = [
+  {
+    x: 52.33376991193947,
+    y: 4.865569696293274,
+    name: 'VU',
+  },
+  {
+    x: 52.32144690826629,
+    y: 4.872101596511191,
+    name: 'Uilenstede 48',
+  },
+  {
+    x: 52.33974766878963,
+    y: 4.842427613789914,
+    name: 'Be Boulder',
+  },
+  {
+    x: 52.358066655876875,
+    y: 4.868508923865922,
+    name: 'Vondel Park',
+  },
+  {
+    x: 52.31184849364095,
+    y: 4.84772881995548,
+    name: 'Lidl',
+  },
+  {
+    x: 52.33137852041053,
+    y: 4.868057053740658,
+    name: 'Free Wheely',
+  },
+  {
+    x: 52.36981757482324,
+    y: 4.880850114081587,
+    name: 'Amsterdam Language Cafe',
+  },
+  {
+    x: 52.30112397858448,
+    y: 4.844430244769097,
+    name: 'Municipality Amstelveen',
+  },
+  {
+    x: 52.35578270872273,
+    y: 4.955728233775735,
+    name: 'UVA',
+  },
+];
+
+const totalPoints = points.length;
+
+interface Genome {}
+
+const genome: Genome = {};
